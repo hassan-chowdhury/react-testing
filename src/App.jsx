63f7689a8e8d3a71648967c0531a199fe504ced3
hassan chowdhury <hassan.chowdhury@sage.com>
2020-03-10 16:00:35 +0000

    testing actions + integration test

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,61 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from './components/header/Header';
 import Headline from './components/headline/Headline';
+import ListItem from './components/listItem/ListItem';
+import SharedButton from './components/button/Button';
+import { fetchPosts } from './actions';
 import './styles.scss';
 
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <section className="main">
-      <Headline header="Posts" desc="Click the button to render posts!" />
-    </section>
-  </div>
-);
+const App = ({
+  aFetchPosts,
+  posts,
+}) => {
+  const fetch = () => {
+    aFetchPosts();
+  };
 
-export default App;
+  return (
+    <div className="App">
+      <Header />
+      <section className="main">
+        <Headline header="Posts" desc="Click the button to render posts!" />
+        <SharedButton buttonText="Get posts" emitEvent={fetch} />
+        {posts.length > 0
+          && (
+            <div>
+              {posts.map((post) => {
+                const { body, title, id } = post;
+                return <ListItem key={id} title={title} desc={body} />;
+              })}
+            </div>
+          )}
+      </section>
+    </div>
+  );
+};
+
+const propTypes = {
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      body: PropTypes.string,
+      id: PropTypes.number,
+      title: PropTypes.string,
+      userId: PropTypes.number,
+    })
+  ),
+  aFetchPosts: PropTypes.func,
+};
+
+App.propTypes = propTypes;
+
+const mapStateToProps = (state) => ({
+  posts: state.posts,
+});
+
+export default connect(
+  mapStateToProps,
+  { aFetchPosts: fetchPosts },
+)(App);
--- src/utils/test.js
+++ src/utils/test.js
@@ -1,7 +1,16 @@
 import checkPropTypes from 'check-prop-types';
+import { applyMiddleware, createStore } from 'redux';
+import { middleware } from '../createStore';
+import rootReducer from '../reducers';
+
 
 const findByTestAttribute = (component, attr) => component.find(`[test="${attr}"]`);
 
 const checkProps = (component, expectedProps) => checkPropTypes(component.propTypes, expectedProps, 'props', component.name);
 
-export { findByTestAttribute, checkProps };
+const testStore = (state) => {
+  const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
+  return createStoreWithMiddleware(rootReducer, state);
+};
+
+export { findByTestAttribute, checkProps, testStore };
